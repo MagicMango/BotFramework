de0ad3200b3f3988218892603430563db4f4eda4
Stefan Sievert <stefan.sievert@stud-informatik.de>
2019-01-12 16:46:18 +0100

    little Discord !chucknorris bug solved configs updated

--- DiscordBot/Commands/JokeCommands.cs
+++ DiscordBot/Commands/JokeCommands.cs
@@ -8,7 +8,7 @@ namespace DiscordBot.Commands
 {
     public class JokeCommands : IWillCommand
     {
-        [Command("!chucknorris"), Description("Sends a random Chuck Norris joke to the chat")]
+        [Command("chucknorris"), Description("Sends a random Chuck Norris joke to the chat")]
         public async Task ChuckNorris(CommandContext ctx)
         {
             await ctx.RespondAsync(WebApiController.GetRandomChuckNorrrisJoke());
--- TwitchBot/Bot.cs
+++ TwitchBot/Bot.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
 using TwitchLib.Api;
 using TwitchLib.Api.Core.Models.Undocumented.Chatters;
 using TwitchLib.Client;
